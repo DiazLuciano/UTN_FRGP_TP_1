fdc6456b277ec9900033381e5c011ee372d3f323
PROBOOK <luchodiaz1995@gmail.com>
2020-08-17 19:46:31 -0300

    primer commit

---
+++ app/src/main/java/com/example/tpn1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.tpn1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Redireccionar_EjercicioUno(View view){
+        Intent redireccion = new Intent(this, Suma.class);
+        startActivity(redireccion);
+    }
+
+    public void Redireccionar_EjercicioDos(View view){
+        Intent redireccion = new Intent(this, Calculadora.class);
+        startActivity(redireccion);
+    }
+}
---
+++ app/src/main/java/com/example/tpn1/Suma.java
@@ -0,0 +1,40 @@
+package com.example.tpn1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Suma extends AppCompatActivity {
+
+    private EditText et1;
+    private EditText et2;
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suma);
+        et1 = (EditText)findViewById(R.id.txtNumeroUno);
+        et2 = (EditText)findViewById(R.id.txtNumeroDos);
+        tv = (TextView)findViewById(R.id.txtResultado);
+    }
+
+    //Este metodo realiza la suma
+    public void Sumar (View view){
+        String valor1 = et1.getText().toString();
+        String valor2 = et2.getText().toString();
+
+        int numeroUno = Integer.parseInt(valor1);
+        int numeroDos = Integer.parseInt(valor2);
+
+        int suma = numeroUno + numeroDos;
+
+        String result = String.valueOf(suma);
+        tv.setText(result);
+    }
+
+
+}
