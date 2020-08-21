63385c9ab65fac5086e15e17b062ab5dee9f09cc
atidele <alejandro.tidele@mercadolibre.com>
2020-08-21 18:23:03 -0300

    Fix in calculator.

--- app/src/main/java/com/example/tpn1/Suma.java
+++ app/src/main/java/com/example/tpn1/Suma.java
@@ -1,11 +1,11 @@
 package com.example.tpn1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Suma extends AppCompatActivity {
 
